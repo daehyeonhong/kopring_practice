5b9980e28ab9913bcff39ac25d2bebb6efebd9e1
DaeHyeonHong <fholng@outlook.com>
2023-01-12 00:33:48 +0900

    [Index] mockUser @withMockUser

--- src/test/kotlin/com/practice/kopring/common/domain/presentation/IndexControllerTests.kt
+++ src/test/kotlin/com/practice/kopring/common/domain/presentation/IndexControllerTests.kt
@@ -1,18 +1,27 @@
 package com.practice.kopring.common.domain.presentation
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(value = [IndexController::class])
 class IndexControllerTests(@Autowired private val mvc: MockMvc) {
 
     @Test
+    @DisplayName(value = "권한")
+    @WithMockUser(username = "guest", roles = ["USER"])
     fun index() {
         mvc.perform(get("/"))
-            .andExpect(status().isOk)
+            .andExpect {
+                status().isOk
+                content().string("ok")
+                println(content())
+            }
     }
 }
