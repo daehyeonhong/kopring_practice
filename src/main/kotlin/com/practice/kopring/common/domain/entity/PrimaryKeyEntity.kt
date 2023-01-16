3ff9cadee0be9b85da24c45a55d100271f5e4666
DaeHyeonHong <fholng@outlook.com>
2023-01-16 22:27:34 +0900

    kotlin jpa 공부

--- src/main/kotlin/com/practice/kopring/common/domain/entity/PrimaryKeyEntity.kt
+++ src/main/kotlin/com/practice/kopring/common/domain/entity/PrimaryKeyEntity.kt
@@ -1,43 +1,64 @@
 package com.practice.kopring.common.domain.entity
 
-import jakarta.persistence.GeneratedValue
+import com.github.f4b6a3.ulid.UlidCreator
+import jakarta.persistence.Column
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import jakarta.persistence.PostLoad
 import jakarta.persistence.PostPersist
 import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.domain.Persistable
+import java.io.Serializable
+import java.time.LocalDateTime
 import java.util.*
 
 @MappedSuperclass
-abstract class PrimaryKeyEntity : Persistable<Long> {
+abstract class PrimaryKeyEntity : Persistable<UUID> {
     @Id
-    @GeneratedValue
-    private val id: Long? = null
+    @Column(columnDefinition = "uuid")
+    private val id: UUID = UlidCreator.getMonotonicUlid().toUuid()
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected var createdAt: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected var updatedAt: LocalDateTime = LocalDateTime.now()
 
     @Transient
     private var _isNew = true
 
-    override fun getId(): Long? = id
+    override fun getId(): UUID = id
 
     override fun isNew(): Boolean = _isNew
 
     override fun equals(other: Any?): Boolean {
-        if (other == null) return false
+        if (other == null) {
+            return false
+        }
 
-        if (other !is HibernateProxy && this::class != other::class) return false
+        if (other !is HibernateProxy && this::class != other::class) {
+            return false
+        }
 
         return id == getIdentifier(other)
     }
 
-    private fun getIdentifier(obj: Any): Long? {
-        return if (obj is HibernateProxy) (obj.hibernateLazyInitializer.implementation as PrimaryKeyEntity).id else (obj as PrimaryKeyEntity).id
+    private fun getIdentifier(obj: Any): Serializable {
+        return if (obj is HibernateProxy) {
+            obj.hibernateLazyInitializer.identifier as Serializable
+        } else {
+            (obj as PrimaryKeyEntity).id
+        }
     }
 
     override fun hashCode() = Objects.hashCode(id)
 
-    @PostPersist
     @PostLoad
+    @PostPersist
     protected fun load() {
         _isNew = false
     }
