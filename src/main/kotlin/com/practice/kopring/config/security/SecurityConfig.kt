84cc36b17ff323c521a61a5a5451dd3a2a5effc8
DaeHyeonHong <fholng@outlook.com>
2023-03-17 20:53:06 +0900

    restAssured를 활용한 oauth2 인증 테스트 4트...

--- src/main/kotlin/com/practice/kopring/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/practice/kopring/config/security/SecurityConfig.kt
@@ -5,6 +5,7 @@ import com.practice.kopring.oauth.filter.JwtFilter
 import com.practice.kopring.oauth.handler.OAuth2SuccessHandler
 import com.practice.kopring.user.application.CustomOAuth2UserService
 import com.practice.kopring.user.application.UserRedisCacheService
+import com.practice.kopring.user.domain.enumerate.Role
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsUtils
 
 @Configuration
 @EnableWebSecurity
@@ -24,25 +26,24 @@ class SecurityConfig constructor(
 
     @Bean
     fun formLoginFilterChain(http: HttpSecurity): SecurityFilterChain {
-        return http.csrf { it.disable() }
+        http.csrf { it.disable() }
             .httpBasic { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.NEVER) }
             .authorizeHttpRequests {
-//                it.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-//                it.requestMatchers("/users/login",).permitAll()
+                it.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                it.requestMatchers("/users/login", "/users/failure").permitAll()
 //                it.requestMatchers(HttpMethod.GET, "/users").permitAll()
-//                it.anyRequest().hasRole(Role.USER.name)
-                it.anyRequest().permitAll()
+                it.anyRequest().hasRole(Role.USER.name)
             }
-            .oauth2Login {
-                it.userInfoEndpoint().userService(this.customOAuth2UserService)
-                it.failureUrl("/users/failure")
-                it.successHandler(this.oAuth2SuccessHandler)
-            }
-            .addFilterBefore(
-                JwtFilter(this.jwtTokenProvider, this.userRedisCacheService),
-                UsernamePasswordAuthenticationFilter::class.java
-            )
-            .build()
+        http.oauth2Login {
+            it.userInfoEndpoint().userService(this.customOAuth2UserService)
+            it.failureUrl("/users/failure")
+            it.successHandler(this.oAuth2SuccessHandler)
+        }
+        http.addFilterBefore(
+            JwtFilter(this.jwtTokenProvider, this.userRedisCacheService),
+            UsernamePasswordAuthenticationFilter::class.java
+        )
+        return http.build()
     }
 }
