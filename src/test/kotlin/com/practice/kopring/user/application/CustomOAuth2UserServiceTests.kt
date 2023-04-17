3f0930ee8edd5e333959c4f7c7bbf5b554e2cee2
DaeHyeonHong <fholng@outlook.com>
2023-04-17 23:03:54 +0900

    oauth2_test

---
+++ src/test/kotlin/com/practice/kopring/user/application/CustomOAuth2UserServiceTests.kt
@@ -0,0 +1,32 @@
+package com.practice.kopring.user.application
+
+import com.practice.kopring.user.infrastructure.UserRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+
+
+@SpringBootTest
+class CustomOAuth2UserServiceTests(
+    @Autowired
+    private val userRepository: UserRepository,
+    @Autowired
+    private val customOAuth2UserService: CustomOAuth2UserService,
+) {
+    @MockBean
+    @Qualifier("defaultOAuth2UserService")
+    private val defaultOAuth2UserService: DefaultOAuth2UserService? = null
+
+    @Test
+    fun loadUserOfNull() {
+        Assertions.assertThrows(OAuth2AuthenticationException::class.java) {
+            customOAuth2UserService.loadUser(null)
+        }
+    }
+
+}
