ef2fea305f2f8502dd832f5bdf4f5c5df4b8a08c
DaeHyeonHong <fholng@outlook.com>
2023-05-21 22:42:41 +0900

    [CI/CD] githubActions 설정 1. blue green deploy 설정

---
+++ scripts/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd /home/ec2-user/app
+
+DOCKER_APP_NAME=spring
+
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+
+echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+
+if [ -z "$EXIST_BLUE" ]; then
+
+  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+  sleep 30
+
+  echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+
+  sudo docker image prune -af
+
+  echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+
+else
+  echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+  sleep 30
+
+  echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+  sudo docker image prune -af
+
+  echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+
+fi
+echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >>/home/ec2-user/deploy.log
+
+echo "===================== 배포 완료 =====================" >>/home/ec2-user/deploy.log
+echo >>home/ec2-user/deploy.log
