b894f746bbc2937a4a60d0a61419d846e03871b2
DaeHyeonHong <fholng@outlook.com>
2023-05-23 23:29:32 +0900

    [CI/CD] test 디버깅

--- src/main/kotlin/com/practice/kopring/config/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/practice/kopring/config/security/SecurityConfiguration.kt
@@ -8,6 +8,7 @@ import com.practice.kopring.user.application.UserRedisCacheService
 import com.practice.kopring.user.enumerate.Role
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -36,7 +37,10 @@ class SecurityConfiguration(
                 it.anyRequest().hasRole(Role.USER.name)
             }
         http.oauth2Login {
-            it.userInfoEndpoint().userService(this.customOAuth2UserService)
+            it.userInfoEndpoint(Customizer.withDefaults())
+                .userInfoEndpoint { t ->
+                    t.userService(this.customOAuth2UserService)
+                }
             it.failureUrl("/users/failure")
             it.successHandler(this.oAuth2SuccessHandler)
         }
