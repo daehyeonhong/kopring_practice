ccc76dace26af46663f4b2f305c18645c20b5f3a
DaeHyeonHong <fholng@outlook.com>
2023-06-18 22:56:54 +0900

    git commit -m ":cookie: :wrench: Change cookie registration and deletion method from jakarta.servlet.http to Spring's ResponseCookie"

--- src/main/kotlin/com/practice/kopring/common/util/CookieUtils.kt
+++ src/main/kotlin/com/practice/kopring/common/util/CookieUtils.kt
@@ -1,38 +1,37 @@
 package com.practice.kopring.common.util
 
-import com.practice.kopring.auth.enumerate.Token
 import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.ResponseCookie
 
-class CookieUtils {
-    companion object {
-        private fun getCookie(request: HttpServletRequest, name: String): Cookie? =
-            request.cookies?.firstOrNull { it.name == name }
-
-        fun addCookie(
-            response: HttpServletResponse,
-            name: String,
-            value: String = "",
-            maxAge: Int = 0
-        ): Unit {
-            Cookie(name, value).let { it: Cookie ->
-                it.secure = true
-                it.isHttpOnly = true
-                it.path = "/"
-                it.maxAge = maxAge
-                response.addCookie(it)
-            }
-        }
-
-        private fun deleteCookie(response: HttpServletResponse, vararg names: String): Unit {
-            names.forEach { name: String ->
-                this.addCookie(response = response, name = name)
-            }
-        }
-
-        fun clearTokenCookies(response: HttpServletResponse): Unit {
-            deleteCookie(response, Token.ACCESS_TOKEN.value, Token.REFRESH_TOKEN.value)
-        }
+object CookieUtils {
+    fun getCookie(request: HttpServletRequest, name: String): Cookie? {
+        val cookies = request.cookies ?: return null
+        return cookies.find { it.name == name }
     }
+
+    fun addCookie(
+        name: String,
+        value: String,
+        maxAge: Long
+    ): ResponseCookie =
+        ResponseCookie.from(name, value)
+            .httpOnly(true)
+            .path("/")
+            .maxAge(maxAge)
+            .build()
+
+    fun deleteCookie(
+        response: HttpServletResponse,
+        name: String
+    ) {
+        val cookie = Cookie(name, "")
+        cookie.path = "/"
+        cookie.maxAge = 0
+        response.addCookie(cookie)
+    }
+
+    fun getValue(request: HttpServletRequest, name: String): String? =
+        getCookie(request, name)?.value?.ifBlank { null }
 }
